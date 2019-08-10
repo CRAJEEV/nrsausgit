98067ccd0a8ce4d80c3188344d9779bc24ae809b
rajeev kumar <rajeev.kumar6@concentrix.com>
2019-08-10 19:19:11 +0530

    Signed-off-by: rajeev kumar <rajeev.kumar6@concentrix.com> file updated chek1

--- dbconnection.js
+++
@@ -1,9 +0,0 @@
-var mysql=require('mysql');
-var connection=mysql.createPool({
-    host: 'sql169.main-hosting.eu.',
-    user: 'u451285657_nrs',
-    password: 'password',
-    database: 'u451285657_nrs',
-    multipleStatements: true
-});
-module.exports=connection;
--- readme.txt
+++
@@ -1,1 +0,0 @@
-https://jinalshahblog.wordpress.com/2016/10/06/rest-api-using-node-js-and-mysql/
